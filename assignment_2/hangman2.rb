3f737f1a48d1284655e531f3e9df744cdf27f343
Sahil <sahilstilak@gmail.com>
2019-12-20 00:27:35 +0530

    Hangman game using methods

---
+++ assignment_2/hangman2.rb
@@ -0,0 +1,43 @@
+words_arr = ['mobile', 'bottle', 'scissor']
+
+def alphabet_input
+  @char = gets.chomp
+end
+
+def includes_alphabet(word)
+  word.include? @char
+end
+
+def alphabet_index_many(word)
+  for j in 0..word.length
+    if word[j] == @char
+      @blank_arr[j] = @char
+    end
+  end
+  print @blank_arr  
+end
+
+words_arr.shuffle.each do |word|
+  @blank_arr = []
+  for j in 1..word.length
+    @blank_arr.push ("_")
+  end
+  n = 5
+  puts word.length
+  while n > 0
+    print "Enter alphabet: "
+    alphabet_input
+    if includes_alphabet(word)
+       alphabet_index_many(word)
+    else
+      n -= 1
+      puts "Number of chances : #{n}"
+    end
+    if word == @blank_arr.join
+      puts "\nSuccess"
+      break
+    end
+  end
+  puts "Fail" if n == 0
+end
+
