abdc0ab7a4db04f330ad256242aef987aa893c3d
Sahil <sahilstilak@gmail.com>
2019-12-22 20:22:42 +0530

    Code for percentage calculation

---
+++ assignment_3/percentage_calc.rb
@@ -0,0 +1,42 @@
+class Result
+  attr_accessor :marks, :name, :id
+  def initialize(marks,name)
+    @marks=marks
+    @name=name
+    @each_percentage = []
+  end
+
+  public
+  def percentage
+    sum= 0
+    for i in 0..3 do
+      sum+=@marks[i].to_f
+    end
+    per = sum/4
+    puts per
+    @each_percentage << per
+  end
+
+  def highest_percent
+    puts @each_percentage.max
+  end
+end
+
+for i in 0..1 do
+  marks=[]
+  puts "Enter name : "
+  name = gets.chomp
+
+  puts "Enter marks",i 
+  for j in 0..3 do
+    marks[j] = gets.to_i
+  end
+
+  obj = Result.new(marks, name)
+  obj.percentage
+end
+obj.highest_percent
+
+
+
+
